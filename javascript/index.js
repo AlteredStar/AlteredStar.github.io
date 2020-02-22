f6ce0c64a66e4cbbc4b673b640a6f6b8d70fc276
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2020-02-22 01:25:39 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,20 @@
 var form = document.querySelector("form");
 var log = document.querySelector("#log");
 
+var str = 0;
+var dex = 0;
+var con = 0;
+var int = 0;
+var wis = 0;
+var cha = 0;
+
 form.addEventListener("submit", function(event) {
     var data = new FormData(form);
     var output = "";
     for (const entry of data) {
-        output = output + entry[0] + "Your stats are: " + entry[1] + "\r";   
+        if (entry[1] == "mansion")
+            str = str + 5;
+        output = output + "str: " + str + "\r" + "dex: " + dex + "\r";   
     };
     log.innerText = output;
     event.preventDefault();
