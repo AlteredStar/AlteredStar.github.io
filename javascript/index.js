f78d531b2c67d79d8803153a7c9dbd6514a61bf7
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2020-02-22 03:44:46 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -39,11 +39,11 @@ function register() {
             }
     }
         output = output 
-            + "Strength: " + str + "<BR>" 
-            + "Dexterity: " + dex + "<BR>" 
-            + "Constitution: " + con + "<BR>"
-            + "Intelligence: " + int + "<BR>"
-            + "Wisdom: " + wis + "<BR>"
-            + "Charisma: " + cha + "<BR>";
+            + "Strength: " + str + "<br>" 
+            + "Dexterity: " + dex + "<br>" 
+            + "Constitution: " + con + "<br>"
+            + "Intelligence: " + int + "<br>"
+            + "Wisdom: " + wis + "<br>"
+            + "Charisma: " + cha + "<br>";
         document.getElementById("result").innerHTML = output;
 }
