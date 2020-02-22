bcb23a32f4dd8975a1ed47ea8413ac6e71926704
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2020-02-22 14:14:55 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -7,11 +7,11 @@ var con = 10;
 var int = 10;
 var wis = 10;
 var cha = 10;
+var ans = 0;
 
 function register() {
     var store = document.forms[0];
     var output = "";
-    var ans = 0;
     for (var i = 0; i < store.length; i++) {
         if (store[i].checked) {
                 if (store[i].value == "forest") {
