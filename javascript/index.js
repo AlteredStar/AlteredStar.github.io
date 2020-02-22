3fdd3315b3741dcad56894bff67b6be3a1188850
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2020-02-22 11:08:18 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -13,12 +13,12 @@ function register() {
     var output = "";
     for (var i = 0; i < store.length; i++) {
             if (store[i].checked) {
-                if (store[i].value == "mansion") {
-                    cha += 5;
-                }
                 if (store[i].value == "forest") {
                     str += 2;
                     con += 3;
+                }
+                if (store[i].value == "mansion") {
+                    cha += 5;
                 }
                 if (store[i].value == "home") {
                     wis += 7;
@@ -52,3 +52,17 @@ function register() {
 function disableButton() {
     document.getElementById("button").disabled = 'true';   
 }
+
+$(":radio").change(function() {
+    var names = {};
+    $(':radio').each(function() {
+        names[$(this).attr('name')] = true;
+    });
+    var count = 0;
+    $.each(names, function() { 
+        count++;
+    });
+    if ($(':radio:checked').length === count) {
+        alert("all answered");
+    }
+}).change();
