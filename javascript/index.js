c470ac17dbff830c78c176ec810dd1969ee8b717
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2020-02-22 00:29:00 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,16 @@ document.getElementById('date').innerHTML = new Date().toDateString();
 function addResponse() {
     document.getElementById("Q1").style.display = 'block'; 
 }
+
+var form = document.querySelector("form");
+var log = document.querySelector("#log");
+
+from.addEventListener("submit", function(event) {
+    var data = new FormData(form);
+    var output = "";
+    for (const entry of data) {
+        output = "Your stats are: ";   
+    };
+    log.innerText = output;
+    event.preventDefault();
+}, false);
