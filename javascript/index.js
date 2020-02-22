6052d8816c09fbf57c8d47ead7552f536e1df981
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2020-02-22 14:24:17 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -53,9 +53,18 @@ function register() {
             + "Intelligence: " + int + "<br>"
             + "Wisdom: " + wis + "<br>"
             + "Charisma: " + cha + "<br>";
-        if (ans == 2) {
+        if (ans > 1) {
             document.getElementById("result").innerHTML = output;
             disableButton();
+        }
+        else {
+            alert("You did not answer all the questions :(");
+            str = 10;
+            dex = 10;
+            con = 10;
+            int = 10;
+            wis = 10;
+            cha = 10;
         }
 }
 
