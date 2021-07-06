9b32ee1043bd5bba8194321c0166de42212b1673
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2021-07-06 02:51:34 -0500

    Update and rename armor.js to weap.js

---
+++ javascript/weap.js
@@ -0,0 +1,32 @@
+var storeWeap = "";
+var weapData = "";
+var name = "";
+var dmg = 0;
+var attackSpeed = 0;
+var range = 0;
+var knockBack = 0;
+var amtAttribute = 0;
+var typeAttribute = "";
+var weight = 0;
+var level = 0;
+var rarity = "";
+var json = "{
+	"name": "Test Longsword",
+	"tier": "Common",
+	"level": 1,
+	"weapon": "Sword",
+	"weight": 30,
+	"material": "wooden_sword",
+	"damage": "1-1",
+	"attackSpeed": 1.4,
+	"attackRange": 3,
+	"knockback": 1.5,
+	"stats": {},
+	"type": "weapon"
+}";
+
+function generate() {
+      
+  
+    document.getElementById("result").innerHTML = json;
+}
