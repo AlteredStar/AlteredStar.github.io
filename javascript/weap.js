ed71a24badf458a3101ccf1b8b43abdbbcaa1a1b
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2021-07-07 09:26:29 -0500

    Update weap.js

--- javascript/weap.js
+++ javascript/weap.js
@@ -1,15 +1,17 @@
 function generate() {
     var storeWeap = "";
     var name = "";
+    var tier = "";
+    var level = 0;
+    var weap = "";
+    var weight = 0;
+    var material = "";
     var dmg = 0;
     var attackSpeed = 0;
     var range = 0;
     var knockBack = 0;
     var amtAttribute = 0;
     var typeAttribute = "";
-    var weight = 0;
-    var level = 0;
-    var rarity = "";
     var json = "";
     
     storeWeap = document.getElementById("weapData").value;
@@ -19,7 +21,7 @@ function generate() {
     //fetch name
     for (let i = 0; i < weapArr.length; i++) {
         var boo = true;
-        boo = !(weapArr[i].charAt(0) > 0);
+        boo = !(weapArr[i].charAt(0) > -1);
         if (i < 4 && boo) {
             name += weapArr[i]
             if (!(weapArr[i+1] > 0))
@@ -27,20 +29,20 @@ function generate() {
         }
     }
     //fetch rarity
-    rarity = weapArr[weapArr.length-1];
+    tier = weapArr[weapArr.length-1];
   
     //compile json skeleton
     json += "{<br>";
     json += "\"name\": \"" + name + "\",<br>";
-    json += "\"tier\": \"" + rarity + "\",<br>";
+    json += "\"tier\": \"" + tier + "\",<br>";
     json += "\"level\": \"" + level + "\",<br>"
-    json += "\"weapon\": \"" + name + "\",<br>"
-    json += "\"weight\": \"" + name + "\",<br>"
-    json += "\"material\": \"" + name + "\",<br>"
-    json += "\"damage\": \"" + name + "\",<br>"
-    json += "\"attackSpeed\": \"" + name + "\",<br>"
-    json += "\"attackRange\": \"" + name + "\",<br>"
-    json += "\"knockback\": \"" + name + "\",<br>"
+    json += "\"weapon\": \"" + weap + "\",<br>"
+    json += "\"weight\": \"" + weight + "\",<br>"
+    json += "\"material\": \"" + material + "\",<br>"
+    json += "\"damage\": \"" + dmg + "\",<br>"
+    json += "\"attackSpeed\": \"" + attackSpeed + "\",<br>"
+    json += "\"attackRange\": \"" + range + "\",<br>"
+    json += "\"knockback\": \"" + knockBack + "\",<br>"
     json += "\"type\": \"" + "weapon" + "\"<br>"
     json += "}";
     
