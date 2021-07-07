2686c0166442aea31c4473e23f86f8e758b868d5
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2021-07-07 02:29:10 -0500

    Update weap.js

--- javascript/weap.js
+++ javascript/weap.js
@@ -1,16 +1,15 @@
-var storeWeap = "";
-var name = "";
-var dmg = 0;
-var attackSpeed = 0;
-var range = 0;
-var knockBack = 0;
-var amtAttribute = 0;
-var typeAttribute = "";
-var weight = 0;
-var level = 0;
-var rarity = "";
-
 function generate() {
+    var storeWeap = "";
+    var name = "";
+    var dmg = 0;
+    var attackSpeed = 0;
+    var range = 0;
+    var knockBack = 0;
+    var amtAttribute = 0;
+    var typeAttribute = "";
+    var weight = 0;
+    var level = 0;
+    var rarity = "";
     var json = "{\"name\": \"";
     
     storeWeap = document.getElementById("weapData").value;
