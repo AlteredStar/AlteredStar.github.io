88bf100e23222f8ec9d1af129216cbfbe8b41081
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2021-09-04 00:47:14 -0500

    Update weap.js

--- javascript/weap.js
+++ javascript/weap.js
@@ -323,7 +323,7 @@ function downloadJson() {
 
 function download(filename, textInput) {
     var element = document.createElement('a');
-    element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
+    element.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(textInput));
     element.setAttribute('download', filename);
     document.body.appendChild(element);
     element.click();
