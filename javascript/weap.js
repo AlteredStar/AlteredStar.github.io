47a7fdae75f77fbbf591dcbcb9a58511b7e88f1e
AlteredStar <55928098+AlteredStar@users.noreply.github.com>
2021-09-03 20:41:40 -0500

    Update weap.js

--- javascript/weap.js
+++ javascript/weap.js
@@ -314,6 +314,7 @@ function generateShield() {
     
     var jsonFileName = "";
     jsonFileName = name;
+    jsonFileName += ".json";
     
     document.getElementById("downloadButton")
         .addEventListener("click", function () {
